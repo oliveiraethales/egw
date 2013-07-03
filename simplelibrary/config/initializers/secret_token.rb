e2a4bb56ef5ce2cfc3a7ba049ba6787199558715
oliveiraethales@gmail.com <oliveiraethales@gmail.com>
2013-07-03 14:59:52 -0300

    Adding base Rails app

---
+++ simplelibrary/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simplelibrary::Application.config.secret_key_base = '9ca8f000f7701a5205daea86746527b6852341159dc92d6d8b4db853df1459dfdcd233ed4ab31d0ee40f81ab91b67a8dcb906cca15e4e0d4ab8cab0ca9d36b46'
