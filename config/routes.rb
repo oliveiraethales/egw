d5c1f0cb1742f981604e6a092e48706c43fbbe87
Thales <oliveiraethales@gmail.com>
2013-12-05 00:56:07 -0200

    I'm not deaddddddd

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Simplelibrary::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root 'high_voltage/pages#show', id: 'index'
 
-  resources :books, :chapters
+  resources :books, :chapters, :categories
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
